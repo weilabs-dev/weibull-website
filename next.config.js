8f7653f3c2211c888134656b932aae19d5f29f34
Alan Anaya <hello@alananaya.dev>
2024-02-18 22:15:10 -0600

    First Commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  trailingSlash: true,
+  reactStrictMode: false,
+  transpilePackages: [
+    '@fullcalendar/common',
+    '@fullcalendar/react',
+    '@fullcalendar/daygrid',
+    '@fullcalendar/list',
+    '@fullcalendar/timegrid'
+  ],
+  experimental: {
+    esmExternals: true
+  },
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
+
+    return config
+  }
+}
